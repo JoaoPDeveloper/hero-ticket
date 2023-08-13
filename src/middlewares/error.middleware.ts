b24547d75206b8e09e72a22a23897c2ac6c57450
JoaoPDeveloper <joao_entreprise@hotmail.com>
2023-08-12 22:08:29 -0300

    add error middlewares e exceptions

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpException } from '../interfaces/HttpExceptions';
+
+export function errorMiddleware(
+  err: HttpException,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const status: number = err.status ?? 500;
+  const message: string = err.message ?? 'Internal Server Error';
+
+  res.status(status).json({
+    message,
+    status,
+  });
+}
+
+// tratativas de errors
